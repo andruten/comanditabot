a85061caa9d5e6b78679d9b7416b75a556ed1746
Andrés Usán <a.usan@i2tic.com>
2021-03-31 00:49:50 +0200

    Command in Spanish

--- commands/weather_in_korea.py
+++ commands/weather_in_korea.py
@@ -8,45 +8,54 @@ from telegram.bot import Bot
 from commands.base import BaseCommandHandler
 
 
-OPEN_WEATHER_MAP_APP_ID = "1234"
-
-
 class WeatherInKoreaCommandHandler(BaseCommandHandler):
     COMMAND_NAME = "tiempoencorea"
+    OPEN_WEATHER_MAP_APP_ID = "1234"
+    WEATHER_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather"
 
     def process(self, update: Update, context: CallbackContext):
         bot: Bot = context.bot
-        bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Dormida... 😡"
-        )
+        if self.is_korea_sleeping():
+            bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="Dormida... 😡"
+            )
         sleep(2)
+        try:
+            weather_data = self.get_weather_data()
+        except ConnectionError:
+            bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="No he podido obtener tiempo 😢",
+            )
+            return
+        weather_text = self.parse_weather_info(weather_data)
         bot.send_message(
             chat_id=update.effective_chat.id,
-            text="No, en serio, ahora te digo."
+            text=weather_text,
         )
-        sleep(1)
+
+    def is_korea_sleeping(self):
+        return True
+
+    def get_weather_data(self):
         query = {
             "q": "Seoul",
-            "appid": OPEN_WEATHER_MAP_APP_ID,
+            "appid": self.OPEN_WEATHER_MAP_APP_ID,
             "units": "metric",
+            "lang": "es",
         }
-        response = requests.get(
-            "https://api.openweathermap.org/data/2.5/weather",
-            params=query,
-        )
-        weather_data = response.json()
-        weather_text = ", ".join([weather.get("main", "") for weather in weather_data.get("weather", [])])
+        response = requests.get(self.WEATHER_ENDPOINT, params=query)
+        return response.json()
+
+    def parse_weather_info(self, weather_data):
+        weather_text = ", ".join([weather.get("description", "")
+                                  for weather in weather_data.get("weather", [])])
         temperature = weather_data.get("main", {})
         temp = temperature.get("temp", 0)
         feels_like = temperature.get("feels_like", 0)
         temp_min = temperature.get("temp_min", 0)
         temp_max = temperature.get("temp_max", 0)
-        bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f"Weather: {weather_text}\n"
-                 f"Temp: {temp}ºC\n"
-                 f"Feels like: {feels_like}ºC\n"
-                 f"Min: {temp_min}ºC\n"
-                 f"Max: {temp_max}ºC",
-        )
+        return f"Hoy hay {weather_text}.\n" \
+               f"Ahora hacen {temp}ºC aunque la sensación térmica es de {feels_like}ºC.\n" \
+               f"La mínima para hoy es de {temp_min}ºC y la máxima de {temp_max}ºC."
