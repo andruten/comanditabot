48812a7be5e167c225c930c4598981f5c95048f3
Andrés Usán <a.usan@i2tic.com>
2021-03-31 01:03:04 +0200

    Refactor commands export

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,1 +1,11 @@
+from .mimimi import MiMiMiCommandHandler
+from .sentenciador import PunisherCommandHandler
+from .star import StarCommandHandler
+from .weather_in_korea import WeatherInKoreaCommandHandler
 
+__all__ = [
+    "MiMiMiCommandHandler",
+    "PunisherCommandHandler",
+    "StarCommandHandler",
+    "WeatherInKoreaCommandHandler",
+]
