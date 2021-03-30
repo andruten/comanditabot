c38415732ab4578b9896c8b82834aa10af7b225e
Andrés Usán <a.usan@i2tic.com>
2021-03-31 01:20:42 +0200

    Refactor mimimi

--- commands/mimimi.py
+++ commands/mimimi.py
@@ -13,13 +13,16 @@ class MiMiMiCommandHandler(BaseCommandHandler):
     def process(self, update: Update, context: CallbackContext):
         bot: Bot = context.bot
         try:
-            text = update.message.reply_to_message.text
+            text = self.do_mimimi(update.message.reply_to_message.text)
         except AttributeError:
-            bot.send_message(
-                chat_id=update.effective_chat.id,
-                text="No puedo hacer mimimi sin citar un mensaje... 😢",
-            )
-            return
+            text = "No puedo hacer _mimimi_ sin citar un mensaje... 😢"
+
+        bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=text,
+        )
+
+    def do_mimimi(self, text):
         text = re.sub('[aeou]', 'i', text, flags=re.I)
         text = re.sub('[AEOU]', 'I', text, flags=re.I)
         text = re.sub('[áéóú]', 'í', text, flags=re.I)
@@ -27,9 +30,4 @@ class MiMiMiCommandHandler(BaseCommandHandler):
         text = re.sub('[àèòù]', 'ì', text, flags=re.I)
         text = re.sub('[ÀÈÒÙ]', 'Ì', text, flags=re.I)
         text = re.sub('[äëöü]', 'ï', text, flags=re.I)
-        text = re.sub('[ÄËÖÜ]', 'Ï', text, flags=re.I)
-
-        bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=text,
-        )
+        return re.sub('[ÄËÖÜ]', 'Ï', text, flags=re.I)
