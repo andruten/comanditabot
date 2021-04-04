0449f3265d10cfce8b61a3da87a48f5ffd4e9587
Andrés Usán <a.usan@i2tic.com>
2021-04-04 23:00:59 +0200

    When try to make mimimi in a mimimied phrase, it turns back to original one

--- commands/mimimi.py
+++ commands/mimimi.py
@@ -1,5 +1,6 @@
 import re
 
+from telegram import ParseMode
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.bot import Bot
@@ -10,13 +11,25 @@ from commands.base import BaseCommandHandler
 class MiMiMiCommandHandler(BaseCommandHandler):
     COMMAND_NAME = "mimimi"
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.mimimis = {}
+
     def process(self, update: Update, context: CallbackContext):
         bot: Bot = context.bot
         try:
             text = self.do_mimimi(update.message.reply_to_message.text)
+            if update.message.reply_to_message.text == text:
+                bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    parse_mode=ParseMode.MARKDOWN,
+                    text=self.mimimis.get(text) or text,
+                )
+                return
+            self.mimimis[text] = update.message.reply_to_message.text
         except AttributeError:
-            text = "No puedo hacer _mimimi_ sin citar un mensaje... 😢"
-
+            text = "No puedo hacer mimimi sin citar un mensaje... 😢"
+        # "store" the translated value for joking purposes
         bot.send_message(
             chat_id=update.effective_chat.id,
             text=text,
