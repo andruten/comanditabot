3a8e9461c905b8dc09047b1802bc6b1e34853d9b
Andrés Usán <a.usan@i2tic.com>
2021-05-01 00:30:28 +0200

    Addedd Rajoy phrases

--- messages/messages.py
+++ messages/messages.py
@@ -1,36 +1,50 @@
-from random import randint
+from random import random, choice
 
+import validators
 from telegram import Update
 from telegram.ext import MessageHandler, Filters, CallbackContext
-import validators
 
-
-class DoNothingException(Exception):
-    pass
+from .constants import RAJOY_PHRASES
+from .exceptions import DoNothingException
 
 
 class Message:
+    PROBABILITY = 20
+
+    def _shall_i_send_it(self):
+        # Only response with a PROBABILITY
+        if random() < self.PROBABILITY / 100.:
+            raise DoNothingException()
+        return True
 
     def __init__(self, message) -> None:
         super().__init__()
-        # Only response with a 20% of probability
-        if randint(1, 5) != 1:
-            raise DoNothingException()
+        self._shall_i_send_it()
         self.message = message
 
     def transform(self):
         raise NotImplementedError()
 
 
+class RajoyMessage(Message):
+    PROBABILITY = 100
+
+    def transform(self):
+        return choice(RAJOY_PHRASES)
+
+
 class PunishmentMessage(Message):
 
     def transform(self):
+        # TODO: Mix with PunisherCommandHandler
         return "Eso tiene, por lo menos, tres días."
 
 
 def message_factory(message):
     if validators.url(message):
         return PunishmentMessage(message).transform()
+    if 'rajoy' in message.lower():
+        return RajoyMessage(message).transform()
     raise DoNothingException()
 
 
@@ -45,5 +59,5 @@ class MessageHandlerFactory(MessageHandler):
         except DoNothingException:
             pass
         else:
-            # Reply to
+            # Reply to message
             update.message.reply_text(text)
