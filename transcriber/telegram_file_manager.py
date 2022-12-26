9b60078e8e22fbf69b91e2164bda72ab211f794c
Andruten <andruten@users.noreply.github.com>
2022-12-26 01:33:01 +0100

    Improved File manager

--- transcriber/telegram_file_manager.py
+++ transcriber/telegram_file_manager.py
@@ -16,7 +16,7 @@ class TelegramAudioManager:
             self,
             context: CallbackContext,
             file: Union[Voice, Audio],
-            file_download_path=None,
+            file_download_path: str = None,
     ) -> None:
         if not isinstance(file, Audio) and not isinstance(file, Voice):
             raise RuntimeError('file must be an Audio or Voice instance')
@@ -25,27 +25,27 @@ class TelegramAudioManager:
         self.context = context
         self.file = file
         self.ogg_audio_path = os.path.join(file_download_path, f'{file.file_unique_id}.ogg')
-        self.mp3_audio_path = f'{self.ogg_audio_path}.mp3'
+        self.mp3_audio_path = os.path.join(file_download_path, f'{file.file_unique_id}.mp3')
 
     def __enter__(self) -> str:
         self._download_voice_message()
+        self._convert_ogg_to_mp3()
         return self.mp3_audio_path
 
-    def __exit__(self, *args, **kwargs):
+    def __exit__(self, *args, **kwargs) -> bool:
         self._clean_up_files()
         return True
 
-    def _clean_up_files(self):
-        logger.debug(f'Removing temporary file {self.mp3_audio_path}')
+    def _clean_up_files(self) -> None:
+        logger.debug(f'Removing temporary files {self.mp3_audio_path} and {self.ogg_audio_path}')
         os.remove(self.mp3_audio_path)
-        logger.debug(f'Removing temporary file {self.ogg_audio_path}')
         os.remove(self.ogg_audio_path)
 
-    def _download_voice_message(self):
+    def _download_voice_message(self) -> None:
+        logger.debug(f'Downloading file')
         new_file = self.context.bot.get_file(self.file.file_id)
         new_file.download(custom_path=self.ogg_audio_path)
-        self._convert_ogg_to_mp3(self.ogg_audio_path, self.mp3_audio_path)
 
-    def _convert_ogg_to_mp3(self, ogg_file_path, mp3_file_path):
-        given_audio = AudioSegment.from_file(ogg_file_path, format="ogg")
-        given_audio.export(mp3_file_path, format="mp3")
+    def _convert_ogg_to_mp3(self) -> None:
+        audio = AudioSegment.from_file(self.ogg_audio_path, format='ogg')
+        audio.export(self.mp3_audio_path, format='mp3')
